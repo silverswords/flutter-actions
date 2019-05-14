a1a0643f608ab2169c0510264ffa3e8f177cfaba
Feng Yifei <fengyfei@gmail.com>
2019-05-14 12:09:29 +0800

    more on BoxDecoration

--- layout/lib/main.dart
+++ layout/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show pi;
 
 void main() => runApp(Application());
 
@@ -12,6 +13,9 @@ class Application extends StatelessWidget {
           children: [
             rowBuilder(context, transformContainer),
             rowBuilder(context, boxDecorationBackgroundImage),
+            rowBuilder(context, boxDecorationBorder),
+            rowBuilder(context, boxDecorationBorderRadius),
+            rowBuilder(context, boxDecorationBoxShadow),
           ],
         ),
       ),
@@ -19,7 +23,12 @@ class Application extends StatelessWidget {
   }
 }
 
+// Definition
 typedef WidgetBuilder = Widget Function(BuildContext);
+
+// Constants
+const EdgeInsetsDirectional marginTop = EdgeInsetsDirectional.only(top: 128);
+const EdgeInsetsDirectional margin = EdgeInsetsDirectional.only(start: 24, top: 128, end: 24);
 
 // row
 Widget rowBuilder(BuildContext context, WidgetBuilder builder) {
@@ -56,10 +65,10 @@ Widget transformContainer(BuildContext context) {
   );
 }
 
-// BoxDecoration
+// BoxDecoration - Background Image
 Widget boxDecorationBackgroundImage(BuildContext context) {
   return Container(
-    margin: const EdgeInsetsDirectional.only(top: 128),
+    margin: marginTop,
     constraints: BoxConstraints.expand(
       height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
     ),
@@ -71,6 +80,90 @@ Widget boxDecorationBackgroundImage(BuildContext context) {
     ),
     alignment: Alignment.center,
     child: Text('Decoration',
+      style: Theme.of(context)
+          .textTheme
+          .display1
+          .copyWith(color: Colors.white)),
+  );
+}
+
+// BoxDecoration - Border
+Widget boxDecorationBorder(BuildContext context) {
+  return Container(
+    margin: margin,
+    constraints: BoxConstraints.expand(
+      height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
+    ),
+    decoration: BoxDecoration(
+      color: Colors.blue[600],
+      border: Border.all(
+        color: Colors.black,
+        width: 8.0,
+      ),
+    ),
+    alignment: Alignment.center,
+    child: Text('Border',
+      style: Theme.of(context)
+          .textTheme
+          .display1
+          .copyWith(color: Colors.white)),
+  );
+}
+
+// BoxDecoration - Border Radius
+Widget boxDecorationBorderRadius(BuildContext context) {
+  return Container(
+    margin: margin,
+    constraints: BoxConstraints.expand(
+      height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
+    ),
+    decoration: BoxDecoration(
+      color: Colors.blue[600],
+      border: Border.all(
+        color: Colors.grey,
+        width: 8.0,
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(24.0)),
+    ),
+    alignment: Alignment.center,
+    child: Text('Border Radius',
+      style: Theme.of(context)
+          .textTheme
+          .display1
+          .copyWith(color: Colors.white)),
+  );
+}
+
+// BoxDecoration - Box Shadow
+Widget boxDecorationBoxShadow(BuildContext context) {
+  return Container(
+    margin: margin,
+    constraints: BoxConstraints.expand(
+      height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
+    ),
+    decoration: BoxDecoration(
+      color: Colors.blue[600],
+      border: Border.all(
+        color: Colors.black,
+        width: 1.0,
+      ),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.purple,
+          offset: Offset.fromDirection(0.25 * pi, 10.0),
+          blurRadius: 4.0,
+          spreadRadius: 4.0,
+        ),
+        BoxShadow(
+          color: Colors.red,
+          offset: Offset.fromDirection(0.25 * pi, -4.0),
+          blurRadius: 4.0,
+          spreadRadius: 2.0,
+        ),
+      ],
+    ),
+    alignment: Alignment.center,
+    child: Text('Border Box Shadow',
       style: Theme.of(context)
           .textTheme
           .display1
